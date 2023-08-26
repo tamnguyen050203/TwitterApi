27a6fa85956cf95f3a18b68b66728726e833a1c4
tamnguyen050203 <tamnguyen050203@gmail.com>
2023-08-26 12:41:31 +0700

    refactor socket server

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,8 @@ import tweetsRouter from '~/routes/tweets.routes'
 import bookmarksRouter from '~/routes/bookmarks.routes'
 import searchRouter from '~/routes/search.routes'
 import { createServer } from 'http'
-import { Server } from 'socket.io'
-import Conversation from '~/models/schemas/Conversations.schema'
 import conversationsRouter from '~/routes/conversations.routes'
-import { ObjectId } from 'mongodb'
-import { verifyAccessToken } from './utils/commons'
-import { UserVerifyStatus } from './constants/enums'
-import { TokenPayload } from './models/requests/User.requests'
-import { ErrorWithStatus } from './models/Errors'
-import { USERS_MESSAGES } from './constants/messages'
-import HTTP_STATUS from './constants/httpStatus'
+import initSocket from './utils/socket'
 
 config()
 databaseService.connect().then(() => {
@@ -48,82 +40,8 @@ app.use('/search', searchRouter)
 app.use('/conversations', conversationsRouter)
 app.use('/static', staticRouter)
 app.use('/static/video', express.static(UPLOAD_VIDEO_DIR))
-
 app.use(defaultErrorHandler)
-
-const io = new Server(httpServer, {
-  cors: {
-    origin: 'http://localhost:3000'
-  }
-})
-const users: {
-  [key: string]: {
-    socket_id: string
-  }
-} = {}
-io.use(async (socket, next) => {
-  const { Authorization } = socket.handshake.auth
-  const access_token = Authorization?.split(' ')[1]
-  try {
-    const decoded_authorization = await verifyAccessToken(access_token)
-    const { verify } = decoded_authorization as TokenPayload
-    if (verify !== UserVerifyStatus.Verified) {
-      throw new ErrorWithStatus({
-        message: USERS_MESSAGES.USER_NOT_VERIFIED,
-        status: HTTP_STATUS.FORBIDDEN
-      })
-    }
-    socket.handshake.auth.decoded_authorization = decoded_authorization
-    socket.handshake.auth.access_token = access_token
-    next()
-  } catch (err) {
-    next({
-      message: 'Unauthorized',
-      name: 'UnauthorizedError',
-      data: err
-    })
-  }
-})
-io.on('connection', (socket) => {
-  console.log(`user ${socket.id} connected`)
-  const { user_id } = socket.handshake.auth.decoded_authorization as TokenPayload
-  users[user_id] = {
-    socket_id: socket.id
-  }
-  socket.use(async (packet, next) => {
-    const { access_token } = socket.handshake.auth
-    try {
-      await verifyAccessToken(access_token)
-    } catch (err) {
-      next(new Error('Unauthorized'))
-    }
-  })
-  socket.on('error', (err) => {
-    if (err.message === 'Unauthorized') {
-      socket.disconnect()
-    }
-  })
-  socket.on('send_message', async (data) => {
-    const { receiver_id, sender_id, content } = data.payload
-    const receiver_socket_id = users[receiver_id]?.socket_id
-    const conversation = new Conversation({
-      sender_id: new ObjectId(sender_id),
-      receiver_id: new ObjectId(receiver_id),
-      content: content
-    })
-    const result = await databaseService.conversations.insertOne(conversation)
-    conversation._id = result.insertedId
-    if (receiver_socket_id) {
-      socket.to(receiver_socket_id).emit('receive_message', {
-        payload: conversation
-      })
-    }
-  })
-  socket.on('disconnect', () => {
-    delete users[user_id]
-    console.log(`user ${socket.id} disconnected`)
-  })
-})
+initSocket(httpServer)
 
 httpServer.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/utils/socket.ts
@@ -0,0 +1,89 @@
+import { ObjectId } from 'mongodb'
+import { verifyAccessToken } from './commons'
+import { UserVerifyStatus } from '../constants/enums'
+import { TokenPayload } from '../models/requests/User.requests'
+import { ErrorWithStatus } from '../models/Errors'
+import { USERS_MESSAGES } from '../constants/messages'
+import HTTP_STATUS from '../constants/httpStatus'
+import { Server } from 'socket.io'
+import Conversation from '~/models/schemas/Conversations.schema'
+import databaseService from '~/services/database.services'
+import { Server as ServerHttp } from 'http'
+
+const initSocket = (httpServer: ServerHttp) => {
+  const io = new Server(httpServer, {
+    cors: {
+      origin: 'http://localhost:3000'
+    }
+  })
+  const users: {
+    [key: string]: {
+      socket_id: string
+    }
+  } = {}
+  io.use(async (socket, next) => {
+    const { Authorization } = socket.handshake.auth
+    const access_token = Authorization?.split(' ')[1]
+    try {
+      const decoded_authorization = await verifyAccessToken(access_token)
+      const { verify } = decoded_authorization as TokenPayload
+      if (verify !== UserVerifyStatus.Verified) {
+        throw new ErrorWithStatus({
+          message: USERS_MESSAGES.USER_NOT_VERIFIED,
+          status: HTTP_STATUS.FORBIDDEN
+        })
+      }
+      socket.handshake.auth.decoded_authorization = decoded_authorization
+      socket.handshake.auth.access_token = access_token
+      next()
+    } catch (err) {
+      next({
+        message: 'Unauthorized',
+        name: 'UnauthorizedError',
+        data: err
+      })
+    }
+  })
+  io.on('connection', (socket) => {
+    console.log(`user ${socket.id} connected`)
+    const { user_id } = socket.handshake.auth.decoded_authorization as TokenPayload
+    users[user_id] = {
+      socket_id: socket.id
+    }
+    socket.use(async (packet, next) => {
+      const { access_token } = socket.handshake.auth
+      try {
+        await verifyAccessToken(access_token)
+      } catch (err) {
+        next(new Error('Unauthorized'))
+      }
+    })
+    socket.on('error', (err) => {
+      if (err.message === 'Unauthorized') {
+        socket.disconnect()
+      }
+    })
+    socket.on('send_message', async (data) => {
+      const { receiver_id, sender_id, content } = data.payload
+      const receiver_socket_id = users[receiver_id]?.socket_id
+      const conversation = new Conversation({
+        sender_id: new ObjectId(sender_id),
+        receiver_id: new ObjectId(receiver_id),
+        content: content
+      })
+      const result = await databaseService.conversations.insertOne(conversation)
+      conversation._id = result.insertedId
+      if (receiver_socket_id) {
+        socket.to(receiver_socket_id).emit('receive_message', {
+          payload: conversation
+        })
+      }
+    })
+    socket.on('disconnect', () => {
+      delete users[user_id]
+      console.log(`user ${socket.id} disconnected`)
+    })
+  })
+}
+
+export default initSocket
